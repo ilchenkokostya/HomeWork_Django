724a8a3f52d1421ebae502ad2db4a235c225edcf
ilchenkokostya <ilchenko.kostya@gmail.com>
2025-01-03 21:38:04 +0300

    vers 1.0.4

---
+++ module_18_4/UrbanDjango/task2/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def task2(request):
+    return render(request, 'second_task/index.html')
+
+def fn_template(request):
+    return render(request, 'second_task/func_template.html')
+
+class cls_template(TemplateView):
+    template_name = 'second_task/class_template.html'
