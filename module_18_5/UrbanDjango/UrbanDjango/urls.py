6872e77edeb3ab5184724f00cf71b8d93c68eec1
KOSTA <ilchenko.kostya@gmail.com>
2025-01-06 21:10:57 +0300

    vers 1.0.4

---
+++ module_18_5/UrbanDjango/UrbanDjango/urls.py
@@ -0,0 +1,39 @@
+"""
+URL configuration for UrbanDjango project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from tempfile import template
+
+from django.contrib import admin
+from django.urls import path
+# from django.views.generic import TemplateView
+from task2.views import *
+# from task3.views import *
+from task4.views import *
+from task5.views import *
+
+
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    path('', index),
+    path('task2/', task2),
+    path('task2/fn', fn_template),
+    path('task2/cls', cls_template.as_view()),
+    # path('cls/', TemplateView.as_view(template_name='class_templates.html'))
+    path('platform/', platform),
+    path('platform/games', menu),
+    path('platform/cart', cart),
+    path('task5/', sign_up_by_html)
+]
---
+++ module_18_5/UrbanDjango/task5/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+from .forms import UserRegister
+users = ['user1', 'user2', 'user3']
+
+
+def sign_up_by_django(request):
+    info = {}
+    if request.method == 'POST':
+        form = UserRegister(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            repeat_password = form.cleaned_data['repeat_password']
+            age = form.cleaned_data['age']
+
+            if username in users:
+                info['error'] = 'Пользователь уже существует'
+            elif age < 18:
+                info['error'] = 'Вы должны быть старше 18'
+            elif password != repeat_password:
+                info['error'] = 'Пароли не совпадают'
+            else:
+                users.append(username)
+                info['message'] = f'Приветствуем, {username}!'
+    else:
+        form = UserRegister()
+        info['message'] = form
+
+    return render(request, 'fifth_task/registration_page.html', info)
+
+
+
+def sign_up_by_html(request):
+    info = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        repeat_password = request.POST.get('repeat_password')
+        age = int(request.POST.get('age', 0))
+
+        if username in users:
+            info['error'] = 'Пользователь уже существует'
+        elif age < 18:
+            info['error'] = 'Вы должны быть старше 18'
+        elif password != repeat_password:
+            info['error'] = 'Пароли не совпадают'
+        else:
+            users.append(username)
+            info['message'] = f'Приветствуем, {username}!'
+    else:
+        info['username'] = request.POST.get('username', '')
+    return render(request, 'fifth_task/registration_page.html', info)
