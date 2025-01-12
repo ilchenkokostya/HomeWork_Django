95eea3a7111379ff056bd1fffc5928f5bcad3c50
KOSTA <ilchenko.kostya@gmail.com>
2025-01-13 00:21:12 +0300

    vers 1.0.4

--- module_19_3/UrbanDjango/task1/views.py
+++ module_19_3/UrbanDjango/task1/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models.functions import Lower
 from .models import Buyer, Game
 from .forms import UserRegister
 
@@ -19,19 +20,27 @@ def cart(request):
 def sign_up_by_html(request):
     info = {}
     users = Buyer.objects.all()
+    print(users)
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         repeat_password = request.POST.get('repeat_password')
         age = int(request.POST.get('age', 0))
 
-        if username in users:
+        if users.filter(name__iexact=username.lower()).exists():
             info['error'] = 'Пользователь уже существует'
         elif age < 18:
             info['error'] = 'Вы должны быть старше 18'
         elif password != repeat_password:
             info['error'] = 'Пароли не совпадают'
         else:
+
+            # запись в таблицу
+            Buyer.objects.create(
+                            name=username,
+                            balance=0,
+                            age=age)
+
             info['message'] = f'Приветствуем, {username}!'
     else:
         info['username'] = request.POST.get('username', '')
