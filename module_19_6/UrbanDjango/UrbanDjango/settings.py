b0b6c62079135b8d84bde3dbf6d2ca6d837eac98
KOSTA <ilchenko.kostya@gmail.com>
2025-01-15 22:59:46 +0300

    vers 1.0.4

--- module_19_6/UrbanDjango/UrbanDjango/settings.py
+++ module_19_6/UrbanDjango/UrbanDjango/settings.py
@@ -73,13 +73,23 @@ WSGI_APPLICATION = 'UrbanDjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',  # Имя базы данных
+        'USER': 'postgres',  # Пользователь PostgreSQL
+        'PASSWORD': '1',  # Пароль PostgreSQL
+        'HOST': 'localhost',  # Адрес сервера базы данных
+        'PORT': '5432',  # Порт подключения
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
